572f946c0205bf2a4d39250b8454cb68e22fc0bb
Zahid <zahid1004048@gmail.com>
2026-02-20 13:08:21 +0600

    next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-   images: {
+  trailingSlash: true,
+  images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-
